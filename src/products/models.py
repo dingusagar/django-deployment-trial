7a8aee6cca5b43ef7e6c8ed8afc22ba430ba24bd
dingusagar <dingusagar@gmail.com>
2018-12-18 15:02:56 +0530

    update

--- src/products/models.py
+++ src/products/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 class Product(models.Model):
     title       = models.CharField(max_length=120) # max_length = required
     description = models.TextField(blank=True, null=True)
-    price       = models.DecimalField(decimal_places=2, max_digits=10000)
+    price       = models.DecimalField(decimal_places=2, max_digits=100)
     summary     = models.TextField(blank=False, null=False)
     featured    = models.BooleanField(default=False) # null=True, default=True
 
